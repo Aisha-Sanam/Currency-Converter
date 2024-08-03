80af4e7919637a23495416ae4f22f0f796bb809c
Aisha-Sanam <aishasanam2132@gmail.com>
2024-08-03 13:39:29 +0500

    first commit

---
+++ currency.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EURO: 301.16,
+    POUND: 351.22,
+    INR: 74.57,
+    PKR: 280
+};
+async function convertCurrency() {
+    let userAnswer = await inquirer.prompt([{
+            name: "from",
+            type: "list",
+            message: "From which currency do you want to convert?",
+            choices: Object.keys(currency)
+        },
+        {
+            name: "to",
+            type: "list",
+            message: "To which currency do you want to convert?",
+            choices: Object.keys(currency)
+        },
+        {
+            name: "amount",
+            type: "number",
+            message: "How much currency do you want to convert?"
+        }]);
+    let result = calculateConversion(userAnswer.from, userAnswer.to, userAnswer.amount);
+    console.log(result);
+}
+function calculateConversion(from, to, amount) {
+    let fromAmount = currency[from];
+    let toAmount = currency[to];
+    let result;
+    switch (from) {
+        case "USD":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "EURO":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "POUND":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "INR":
+            result = amount / fromAmount * toAmount;
+            break;
+        case "PKR":
+            result = amount / fromAmount * toAmount;
+            break;
+        default:
+            result = "Invalid currency";
+    }
+    return result;
+}
+convertCurrency();
